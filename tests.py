b5b66fb0c55b0dfa5ee294b284a1c8086db663b5
Fellipe Souto <fllsouto@gmail.com>
2026-03-26 23:54:10 -0300

    Commit 3: Testing with fixtures

--- tests.py
+++ tests.py
@@ -1,18 +1,29 @@
 import pytest
 from model import Question
 
-# t1
+@pytest.fixture
+def question():
+    return Question(title='q1')
+
+@pytest.fixture
+def question_with_choices(question):
+    question.add_choice('a', False)
+    question.add_choice('b', True)
+    question.add_choice('c', False)
+    return question
+
+# t1: create question with valid title
 def test_create_question():
     question = Question(title='q1')
     assert question.id != None
 
-#t2
+# t2: create multiple questions with unique ids
 def test_create_multiple_questions():
     question1 = Question(title='q1')
     question2 = Question(title='q2')
     assert question1.id != question2.id
 
-#t3
+# t3: create question with invalid title
 def test_create_question_with_invalid_title():
     with pytest.raises(Exception):
         Question(title='')
@@ -21,18 +32,15 @@ def test_create_question_with_invalid_title():
     with pytest.raises(Exception):
         Question(title='a'*500)
 
-#t4
+# t4: create question with valid points
 def test_create_question_with_valid_points():
     question = Question(title='q1', points=1)
     assert question.points == 1
     question = Question(title='q1', points=100)
     assert question.points == 100
 
-
-#t5
-def test_create_choice():
-    question = Question(title='q1')
-    
+# t5: create choice for question
+def test_create_choice(question):
     question.add_choice('a', False)
 
     choice = question.choices[0]
@@ -40,7 +48,7 @@ def test_create_choice():
     assert choice.text == 'a'
     assert not choice.is_correct
 
-# t6 : create question with invalid points
+# t6: create question with invalid points
 def test_create_question_with_invalid_points():
     error_message = "Points must be between 1 and 100"
 
@@ -53,12 +61,11 @@ def test_create_question_with_invalid_points():
     assert error_message in str(exc.value)
 
 # t7: create choice with invalid text size |a| < |text| < |b|
-def test_create_choice_with_invalid_text_size():
+def test_create_choice_with_invalid_text_size(question):
     message_a = ""
     message_b = "a"*101
     error_message_a = 'Text cannot be empty'
     error_message_b = 'Text cannot be longer than 100 characters'
-    question = Question(title='q1')
 
     with pytest.raises(Exception) as exc:
         question.add_choice(message_a, False)
@@ -69,7 +76,7 @@ def test_create_choice_with_invalid_text_size():
     assert error_message_b in str(exc.value)
 
 # t8: create question with multiple choices
-def test_create_question_with_multiple_questions():
+def test_create_question_with_multiple_questions(question):
     choice_list = [
         ('a', False),
         ('b', False),
@@ -77,8 +84,6 @@ def test_create_question_with_multiple_questions():
         ('d', False),
         ('e', False),
     ]
-
-    question = Question(title='q1')
 
     for choice in choice_list:
         question.add_choice(choice[0], choice[1])
@@ -128,15 +133,10 @@ def test_select_more_choices_in_multi_select_question():
         assert correct_choice in selected_correct_choices
 
 # t12: remove all choices from question
-def test_remove_all_choices_from_question():
-    question = Question(title='q1')
-    question.add_choice('a', False)
-    question.add_choice('b', True)
-    question.add_choice('c', True)
+def test_remove_all_choices_from_question(question_with_choices):
+    question_with_choices.remove_all_choices()
 
-    question.remove_all_choices()
-
-    assert len(question.choices) == 0
+    assert len(question_with_choices.choices) == 0
 
 
 # t13: set one correct choice for question
@@ -157,8 +157,7 @@ def test_set_correct_choice_for_question():
 
 
 # t14: set wrong correct choice for questions
-def test_set_wrong_correct_choice_for_questions():
-    question = Question(title='q1')
+def test_set_wrong_correct_choice_for_questions(question):
     mapped_choice_id = question.add_choice('a', False).id
     unmaped_choice_id = 99
     error_message = f'Invalid choice id {unmaped_choice_id}'
@@ -169,8 +168,7 @@ def test_set_wrong_correct_choice_for_questions():
     assert error_message in str(exc.value)
 
 # t15: remove choice by id
-def test_set_wrong_correct_choice_for_questions():
-    question = Question(title='q1')
+def test_remove_choice_by_id(question):
     question.add_choice('a', False)
     removed_choice_id = question.add_choice('b', False).id
     question.add_choice('c', False)
